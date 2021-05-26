219332736f8efe045fb43491c433fd5b858fb45e
Fred Baine <fred.baine@staplcotn.com>
2021-05-26 17:17:53 -0500

    cgwa

--- cgwa.py
+++ cgwa.py
@@ -6,6 +6,8 @@ import Preprocess
 
 print(cv2.__version__)
 
+boxes = []
+
 # Globals
 lh = 0
 ls = 0
@@ -25,17 +27,16 @@ def nothing(x):
 def processImage(frame):
     frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     thresh = cv2.inRange(frame_HSV, (lh, ls, lv), (hh, hs, hv))
-    cv2.imwrite('thresh.jpg', smaller(thresh))
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     tags = findTags(contours)
     print('Tags found: ',  len(tags))
     for i, tag in enumerate(tags):
         roi = tag.ROI = frame[tag.intY : tag.intY + tag.intHeight,
                            tag.intX : tag.intX + tag.intWidth]
-        cv2.imshow('roi', larger(roi))
+        # cv2.imshow('roi', larger(roi))
         cv2.drawContours(frame, tag.contour, -1, (0,0,255), 3)
-        cv2.putText(frame, str(i), (tag.intX, tag.intY), font, fontScale, fontColor, fontThickness )      
-        cv2.waitKey(0)
+        # cv2.putText(frame, str(i), (tag.intX, tag.intY), font, fontScale, fontColor, fontThickness )      
+        # cv2.waitKey(0)
 
     return frame
 
@@ -90,6 +91,20 @@ def smaller(image):
 def larger(image):
     return cv2.resize(image, None, fx=3, fy=3, interpolation=cv2.INTER_AREA)
 
+def on_mouse(event, x, y, flags, params):
+   # global img
+
+   if event == cv2.EVENT_LBUTTONDOWN:
+      print('Start Mouse Position: ', str(x), str(y))
+      sbox = [x, y]
+      boxes.append(sbox)
+
+   elif event == cv2.EVENT_LBUTTONUP:
+      print('End Mouse Position: ', str(x), str(y))
+      ebox = [x, y]
+      boxes.append(ebox)
+
+
 cv2.namedWindow('Original')
 cv2.moveWindow('Original', 0, 0)
 # cv2.namedWindow('Thresh')
@@ -98,22 +113,32 @@ cv2.moveWindow('Original', 0, 0)
 # cv2.moveWindow('eq', 700, 0)
 
 
-# path = './images/bales/bb*.*'
-# images = os.listdir(path)
-images =  glob.glob('./images/bales/bb_ot4.*')
-for idx, image in enumerate(images):
-    print('Processing image ', str(idx), '-', image)
-    frame = cv2.imread(image)
-    if frame is None:
-        print('File Not found: ', imagePath)
-        quit()
-    frame = processImage(frame)
-    cv2.imshow('Original', smaller(frame))   
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+imagePath = './images/bales/bb_ot4.jpg'
+frame = cv2.imread(imagePath)
+overlay = frame.copy()
+alpha = 0.4
 
+if frame is None:
+    print('File Not found: ', imagePath)
+    quit()
+#frame = processImage(frame)
+cv2.rectangle(overlay, (292*4,245*4), (419*4,587*4), (0,255,0), -1)
+cv2.rectangle(overlay, (429*4,593*4), (561*4,916*4), (0,0,255), -1)
+frame = cv2.addWeighted(overlay, alpha, frame, 1.0 - alpha, 0)    
+cv2.putText(frame, "1211562", (292*4+75,245*4+100), 
+    cv2.FONT_HERSHEY_SIMPLEX, 3.0, (0, 0, 255), 7)
+cv2.putText(frame, "1211562", (429*4+75, 593*4+100), 
+    cv2.FONT_HERSHEY_SIMPLEX, 3.0, (0, 0, 255), 7)
+cv2.imshow('Original', smaller(frame))   
 
-# if cv2.waitKey(1)==ord('q'):
-#     break
+while True:
+    cv2.setMouseCallback('Original', on_mouse, 0)
+    kp = cv2.waitKey(1)
+    if kp > 0:
+        print('key pressed: ', kp)
+    if kp==ord('q'):
+        break
+
+cv2.imwrite('cgwa.jpg', frame)
+# cv2.waitKey(0)
 cv2.destroyAllWindows()
-
